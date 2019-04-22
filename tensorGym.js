ff30b5ae0ddaa1d98def617e8b1873fa5c0f3eaa
Mustafa Sohail <mustafasohail07@gmail.com>
2019-04-22 17:55:25 -0500

    Posenet loaded with script cdn

--- tensorGym.js
+++ tensorGym.js
@@ -1,3 +1,9 @@
+var imageScaleFactor = 0.5;
+var outputStride = 16;
+var flipHorizontal = false;
+
+posenet.load().then((net) => console.log(net))
+
 var video = document.querySelector("#video");
 
 if (navigator.mediaDevices.getUserMedia) {
@@ -17,3 +23,21 @@ function playSound() {
 var bb = document.getElementById("buzzerButton")
 
 bb.addEventListener('click', playSound)
+
+
+
+
+
+  // <script>
+  //   var imageScaleFactor = 0.5;
+  //   var outputStride = 16;
+  //   var flipHorizontal = false;
+
+  //   var imageElement = document.getElementById('cat');
+
+  //   posenet.load().then(function(net){
+  //     return net.estimateSinglePose(imageElement, imageScaleFactor, flipHorizontal, outputStride)
+  //   }).then(function(pose){
+  //     console.log(pose);
+  //   })
+  // </script>
